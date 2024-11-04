ba029798a61eaa515ba6ed3f726b6c5b92befc15
burcuicen <burcuicen2000@gmail.com>
2024-11-05 00:37:49 +0300

    feat:add custom template urls

--- editor/src/services/constants/index.js
+++ editor/src/services/constants/index.js
@@ -12,9 +12,63 @@ export const API = {
 };
 
 export const TEMPLATE_URLS = {
-  html: "https://raw.githubusercontent.com/ardas/stripo-plugin/master/Public-Templates/Basic-Templates/Trigger%20newsletter%20mockup/Trigger%20newsletter%20mockup.html",
-  css: "https://raw.githubusercontent.com/ardas/stripo-plugin/master/Public-Templates/Basic-Templates/Trigger%20newsletter%20mockup/Trigger%20newsletter%20mockup.css",
+  html: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Basic-Templates/New-editor-template/New-editor-template.html",
+  css: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Basic-Templates/New-editor-template/New-editor-template.css",
 };
+export const TEMPLATES_BY_CATEGORY = [
+  {
+    category: "Cars",
+    templates: [
+      {
+        name: "Father's Day",
+        html: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Cars/Fathers-day-newsletter/Fathers-Day.html",
+        css: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Cars/Fathers-day-newsletter/Fathers-Day.css",
+        thumbnail:
+          "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Cars/Fathers-day-newsletter/Fathers-Day.png",
+      },
+      {
+        name: "Promo",
+        html: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Cars/Promo-newsletter/Promo-newsletter.html",
+        css: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Cars/Promo-newsletter/Promo-newsletter.css",
+        thumbnail:
+          "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Cars/Promo-newsletter/Promo-newsletter.png",
+      },
+      {
+        name: "Trigger",
+        html: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Cars/Trigger-newsletter/Trigger-newsletter.html",
+        css: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Cars/Trigger-newsletter/Trigger-newsletter.css",
+        thumbnail:
+          "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Cars/Trigger-newsletter/Trigger-newsletter.png",
+      },
+    ],
+  },
+  {
+    category: "Fashion",
+    templates: [
+      {
+        name: "Black Friday",
+        html: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Fashion/Black-friday/Black-friday.html",
+        css: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Fashion/Black-friday/Black-friday.css",
+        thumbnail:
+          "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Fashion/Black-friday/Black-friday.png",
+      },
+      {
+        name: "Promo",
+        html: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Fashion/Promo-newsletter/Promo-newsletter.html",
+        css: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Fashion/Promo-newsletter/Promo-newsletter.css",
+        thumbnail:
+          "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Fashion/Promo-newsletter/Promo-newsletter.png",
+      },
+      {
+        name: "Trigger",
+        html: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Fashion/Trigger-newsletter/Trigger-newsletter.html",
+        css: "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Fashion/Trigger-newsletter/Trigger-newsletter.css",
+        thumbnail:
+          "https://raw.githubusercontent.com/ardas/stripo-plugin/refs/heads/master/Public-Templates/Custom-Templates/Fashion/Trigger-newsletter/Trigger-newsletter.png",
+      },
+    ],
+  },
+];
 
 export const EDITOR_CONFIG = {
   containers: {
